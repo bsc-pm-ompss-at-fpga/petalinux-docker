775afdc20f6f8010e7d4f03833e6c4a852a952bd
Miquel Vidal i Pinyol <miquel.vidal@bsc.es>
2026-05-28 11:19:06 +0200

    Fixes for 2021.2

--- docker/user_entrypoint.sh
+++ docker/user_entrypoint.sh
@@ -3,6 +3,9 @@
 export SHELL="/bin/bash"
 export DL_DIR=/yocto/dl
 export SSTATE_DIR=/yocto/ss
+export HOME=/builder
+
+echo 'export PS1='\''\[\e[0m\][\[\e[34m\]\w\[\e[0m\]] > '\''' >> ~/.bashrc
 
 source /opt/petalinux/settings.sh /opt/petalinux
 
